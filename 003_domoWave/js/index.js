8fca5f47bd916452b49181656e43a6d75876868d
HouLijun <754846039@qq.com>
2017-07-19 09:32:18 +0800

    Update index.js

--- 003_domoWave/js/index.js
+++ 003_domoWave/js/index.js
@@ -6,6 +6,8 @@ window.onload=function () {
         //画布属性
         var cw = canvas.width ;
         var ch = canvas.height ;
+        //圆属性
+        var radius=150;
 
         //圆
         function outsideCircle(r) {
@@ -21,30 +23,27 @@ window.onload=function () {
             ctx.arc(0, 0, r-10, 0, 2 * Math.PI);
             ctx.closePath();
             ctx.restore();
-            ctx.clip();
+            // ctx.clip();
         }
 
         //Sin 曲线属性
         var sinParam1={
-            nowRange:52, //波浪的高度
-            sinX : 0,
-            sinY : ch / 2,
-            axisLength : cw, //轴长
-            waveWidth : 0.015 ,   //波浪宽度,数越小越宽
-            waveHeight :10, //波浪高度,数越大越高
-            speed :0.06, //波浪速度，数越大速度越快
-            xOffset: 0 //波浪x偏移量
+            range:0,             //波浪的高度(占画布高的百分比，用来计算y轴位移)
+            sinX : -radius,               //起始点x
+            axisLength : 2*radius,        //轴长
+            palstance : 0.015 ,     //波浪宽度,数越小越宽(角速度)
+            amplitude :10,         //波浪高度,数越大越高(振幅)
+            speed :0.06,            //波浪速度，数越大速度越快
+            xOffset: 0              //波浪x偏移量（初相）
         }
-
         var sinParam2={
-            nowRange:51.9,
-            sinX :50,
-            sinY : ch / 2,
-            axisLength : cw, //轴长
-            waveWidth : 0.011 ,   //波浪宽度,数越小越宽
-            waveHeight :10, //波浪高度,数越大越高
-            speed :0.06, //波浪速度，数越大速度越快
-            xOffset: 3 //波浪x偏移量
+            range:3, //波浪的高度(占画布高的百分比，用来计算y轴位移)
+            sinX : -radius,   //起始点x
+            axisLength : 2*radius,        //轴长
+            palstance : 0.01 ,     //波浪宽度,数越小越宽(角速度)
+            amplitude :10,         //波浪高度,数越大越高(振幅)
+            speed :0.06,            //波浪速度，数越大速度越快
+            xOffset: 10              //波浪x偏移量（初相）
         }
 
 
@@ -52,38 +51,37 @@ window.onload=function () {
         //绘制sin曲线
         function drawSin(sinParam,color){
             ctx.save();
-
-            var points=[];  //用于存放绘制Sin曲线的点
-
+            //画笔移到中心点
+            ctx.translate(cw/2,ch/2);
             ctx.beginPath();
             //在整个轴长上取点
-            for(var x = sinParam.sinX; x < sinParam.sinX + sinParam.axisLength; x += 20 / sinParam.axisLength){
-                //此处坐标(x,y)的取点，依靠公式 “振幅高*sin(x*振幅宽 + 振幅偏移量)”
-                var y = -Math.sin((sinParam.sinX + x) * sinParam.waveWidth + sinParam.xOffset);
-
-                var dY = ch * (1 - sinParam.nowRange / 100 );
-
-                points.push([x, dY + y * sinParam.waveHeight]);
-                ctx.lineTo(x, dY + y * sinParam.waveHeight);
+            for(var x = sinParam.sinX; x < sinParam.sinX + sinParam.axisLength; x++){
+                //y=振幅*sin(x*角速度 + 振幅偏移量)+y轴偏移量
+                var y=sinParam.amplitude*Math.sin(x*sinParam.palstance+sinParam.xOffset)+sinParam.range;
+                ctx.lineTo(x, y);
             }
 
             //封闭路径
-            ctx.lineTo(sinParam.axisLength, ch);
-            ctx.lineTo(sinParam.sinX, ch);
-            ctx.lineTo(points[0][0],points[0][1]);
+            ctx.lineTo(radius, radius);
+            ctx.lineTo(sinParam.sinX, radius);
+            ctx.closePath();
             ctx.fillStyle = color;
             ctx.fill();
             ctx.restore();
         };
         //动画
+        var t;
         function draw() {
+            clearTimeout(t);
             ctx.clearRect(0, 0, cw, ch);
-            outsideCircle(150);
+            outsideCircle(radius);
             drawSin(sinParam1,"#96b4d3");
             drawSin(sinParam2,"rgba(94,150,202,0.8)");
-            sinParam1.xOffset += sinParam1.speed;
-            sinParam2.xOffset += sinParam2.speed;
-            requestAnimationFrame(draw);
+            sinParam1.xOffset -= sinParam1.speed;
+            sinParam2.xOffset -= sinParam2.speed;
+            t=setTimeout(function () {
+                draw();
+            },20);
         }
         draw();
     }else {
